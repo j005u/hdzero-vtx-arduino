62a69ec4d66ed682992f7355bbb87c230eee36bc
Ben Lumley <ben@benlumley.co.uk>
2023-07-19 09:06:38 +0100

    Hacked basicexample into a useful shape for fpv.wtf long run tests

--- examples/BasicExample/BasicExample.ino
+++ examples/BasicExample/BasicExample.ino
@@ -6,14 +6,17 @@ HDZero hdzero;
 
 void setup() {
     // wait for serial to be ready
-    Serial2.begin(115200);
-    while (!Serial2)
+    Serial.begin(115200);
+    while (!Serial)
         ;
 
     // setup and configure hdzero
-    hdzero.begin(Serial2);
+    hdzero.begin(Serial);
     hdzero.setFcVariant("INAV");
-    hdzero.setResolution(HD_5018);
+    hdzero.setResolution(SD_3016);
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, 0);
 }
 
 uint32_t lastRefreshTime = 0;
@@ -24,20 +27,47 @@ uint8_t pointX = 0;
 int8_t pointYDirection = 1;
 uint8_t pointY = 0;
 
+unsigned long armLoopStartTime = 0;
+
+char loopTime[50];
+
+
 void loop() {
     if (millis() - lastRefreshTime >= 500) {
         hdzero.clear();
-        hdzero.writeString(0, 10, 10, "HELLO ARDUINO!");
+        hdzero.writeString(0, 1, 10, "Tick tock!");
         hdzero.writeString(0, pointX, pointY, "AHHH");
 
+        sprintf(loopTime, "%ld", millis()-armLoopStartTime);
+
+        hdzero.writeString(0, 1, 11, loopTime);
+        hdzero.writeString(0, 1, 12, hdzero.getArmed() ? "ARMED" : "DISARMED");
+
         pointX += pointXDirection;
-        if (pointX + 4 >= 50 || pointX == 0) {
+        if (pointX + 4 >= 30 || pointX == 0) {
             pointXDirection = -pointXDirection;
         }
         pointY += pointYDirection;
-        if (pointY >= 18 || pointY == 0) {
+        if (pointY >= 16 || pointY == 0) {
             pointYDirection = -pointYDirection;
         }
+
+        // read this from bottom to top
+        if (millis() - armLoopStartTime >= 439000) { // disarm and reset after 7 minutes
+            hdzero.setArmed(false);
+            armLoopStartTime = millis();
+            digitalWrite(LED_BUILTIN, 0);
+        } else if (millis() - armLoopStartTime >= 19000) { // rearm after another 3 seconds
+            hdzero.setArmed(true);
+            digitalWrite(LED_BUILTIN, 1);
+        } else if (millis() - armLoopStartTime >= 16000) { // then disarm after a further 6
+            hdzero.setArmed(false);
+            digitalWrite(LED_BUILTIN, 0);
+        } else if (millis() - armLoopStartTime >= 10000) { // arm after ten seconds
+            hdzero.setArmed(true);
+            digitalWrite(LED_BUILTIN, 1);
+        }
+
         hdzero.draw();
 
         lastRefreshTime = millis();
